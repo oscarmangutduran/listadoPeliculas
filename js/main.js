5fe36cf8e5d3b774237981bb6b42279b69b15a4e
Oscar Mangut <mangutduranoscar@gmail.com>
2024-09-11 18:44:32 +0200

    App y load

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import App from "./src/peliculas/app.js";
+
+//crear el objeto app
+const peliculaApp = new App();
+
+//cargar toda la app
+peliculaApp.load();
---
+++ js/src/peliculas/app.js
@@ -0,0 +1,12 @@
+export default class App {
+  constructor() {}
+  load() {
+    //Añadir pelicula
+
+    //Listar peliculas
+
+    //Buscar peliculas
+
+    console.log("La aplicación de películas ha sido inicializada");
+  }
+}
