9f4042ba4703472c347ae2c312fb73c0ebe4b2ea
Oscar Mangut <mangutduranoscar@gmail.com>
2024-09-12 18:03:28 +0200

    Listado al cargar la app

--- js/src/peliculas/app.js
+++ js/src/peliculas/app.js
@@ -1,12 +1,24 @@
 import Add from "./modules/add.js";
+import List from "./modules/list.js";
+import Storage from "./modules/storage.js";
 
 export default class App {
   constructor() {
     // Inicializar la clase Add
     this.add = new Add();
+    this.list = new List();
+    this.Storage = new Storage();
   }
 
   load() {
+    //Añadir pelicula
+    this.add.peliculaSave();
+
+    //Conseguir array objetos localStorage
+    const pelicualas = this.Storage.getData();
+
+    //Listar peliculas
+    this.list.show(pelicualas);
     // Añadir película (este método ya se llama automáticamente en el constructor de Add)
     console.log("La aplicación de películas ha sido inicializada");
   }
